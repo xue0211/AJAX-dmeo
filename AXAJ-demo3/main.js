e66284d7c3469886b5ab54caa76615b2fdbbd1ba
xue <2723709118@qq.com>
2024-07-12 15:38:22 +0800

    使用promise

--- AXAJ-demo3/main.js
+++ AXAJ-demo3/main.js
@@ -7,8 +7,26 @@ window.jQuery = function (nodeOrSelector) {
 
 window.$ = window.jQuery
 
-window.jQuery.ajax = function ({url, method, body, successFn, failFn, headers}) {
-   // let url = options.url
+window.jQuery.ajax = function ({ url, method, body, headers }) {
+    return new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest()
+        request.open(method, url) //配置 request
+        for (let key in headers) {
+            let value = headers[key]
+            request.setRequestHeader(key, value)
+        }
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                } else if (request.status >= 400) {
+                    reject.call(undefined, request)
+                }
+            }
+        }
+        request.send(body)
+    })
+    // let url = options.url
     /*let url
     if (arguments.length === 1) {
         url = options.url
@@ -23,47 +41,29 @@ window.jQuery.ajax = function ({url, method, body, successFn, failFn, headers}) 
     let headers = options.headers*/
     // ES6 解构赋值 --> 分析它的结构，然后赋值
 
-    let request = new XMLHttpRequest()
-    request.open(method, url) //配置 request
-    for (let key in headers) {
-        let value = headers[key]
-        request.setRequestHeader(key, value)
-    }
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                successFn.call(undefined, request.responseText)
-            } else if (request.status >= 400) {
-                failFn.call(undefined, request)
-            }
-        }
-    }
-    request.send(body)
+
 }
 
-function f1(responseText) { }
-function f2(responseText) { }
 
 myButton.addEventListener('click', (e) => {
-    window.jQuery.ajax({
-        url: '/xue',
+    let Promise = window.jQuery.ajax({
+        url: '/xxx',
         method: 'get',
         headers: {
             'content-type': 'application/x-www-form-urlencoded',
             'frank': '18'
         },
-        successFn: (x) => {
-            //console.log(x) //callback --> responseText
-            f1.call(undefined, x)
-            f2.call(undefined, x)
-        },
-        failFn: (x) => {
-            console.log(x) //callback --> request
-            console.log(x.status)
-            console.log(x.responseText)
-        }
+
     })
+    Promise.then(
+        (text) => { console.log(text) },   //成功
+        (request) => { console.log(request) }   //失败
+    )
 })
+
+/*return new promise(function(resolve,reject)){
+
+}
 
 /*
 jQuery.ajax({
