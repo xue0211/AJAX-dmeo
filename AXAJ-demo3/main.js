3026de97af8a6cfdac118771f0ac9d1b99ba4ebf
xue <2723709118@qq.com>
2024-07-12 10:53:04 +0800

    用jQuery封装AJAX

--- AXAJ-demo3/main.js
+++ AXAJ-demo3/main.js
@@ -7,19 +7,21 @@ window.jQuery = function (nodeOrSelector) {
 
 window.$ = window.jQuery
 
-window.jQuery.ajax = function (options) {
-    let url
+window.jQuery.ajax = function ({url, method, body, successFn, failFn, headers}) {
+   // let url = options.url
+    /*let url
     if (arguments.length === 1) {
         url = options.url
     } else if (arguments.length === 2){
         url = arguments[0]
         options = arguments[1]
-    }
-    let method = options.method
+    }*/
+    /*let method = options.method
     let body = options.body
     let successFn = options.successFn
     let failFn = options.failFn
-    let headers = options.headers
+    let headers = options.headers*/
+    // ES6 解构赋值 --> 分析它的结构，然后赋值
 
     let request = new XMLHttpRequest()
     request.open(method, url) //配置 request
@@ -62,3 +64,28 @@ myButton.addEventListener('click', (e) => {
         }
     })
 })
+
+/*
+jQuery.ajax({
+    success: ()=>{},
+    error: ()=>{}
+})
+
+frank.ajax({
+    成功: function(){},
+    失败: function(){}
+})
+
+Image.onload = function(){}
+Image.onerror = function(){}
+
+jack.ajax(null, null, null, successFn, failFn)
+
+fs.readFile(function(error, content){
+  if(error){
+    //失败
+  }else{
+    //成功
+  }
+})
+  */
